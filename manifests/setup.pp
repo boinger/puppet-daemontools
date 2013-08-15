9926f7ad7d8438c8e549fba7601eb17524333792
jv <jeff@jeffvier.com>
2013-08-15 13:20:47 -0700

    syntax compression

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -25,6 +25,10 @@ define daemontools::setup(
     'stopped' => 'absent',
   }
 
+  Exec {
+    path => ["/bin", "/usr/bin", "/usr/local/bin"],
+  }
+
   if (!defined(File["/etc/${name}"])){  ## Often, the base /etc/whatever is already defined.  No need to be bossy about it.
     file {
     "/etc/${name}":
@@ -35,28 +39,20 @@ define daemontools::setup(
 
   exec {
     "restart ${name}":
-      path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:",
       command     => "svc -t /etc/${name}",
       refreshonly => true;
 
     "restart ${name} log":
-      path        => "/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:",
       command     => "svc -t /etc/${name}/log",
       refreshonly => true;
   }
 
   file {
-    "/etc/${name}/log":
-      ensure  => $dir,
-      owner   => $user,
-      mode    => 2755;
-
-    "/etc/${name}/env":
-      ensure  => $dir,
-      owner   => $user,
-      mode    => 2755;
-
-    "/etc/${name}/supervise":
+    [
+    "/etc/${name}/log",
+    "/etc/${name}/env",
+    "/etc/${name}/supervise",
+    ]:
       ensure  => $dir,
       owner   => $user,
       mode    => 2755;
